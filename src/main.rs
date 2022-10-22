63101414730561f20a4165f69d6c4f1be9386766
kn <kvasarnomad@Shadow.diaspora>
2022-10-23 00:48:45 +0200

    contact with chip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+const FIRST_CMD: [u8; 1] = [0x60];
+
 fn main() -> nfc1::Result<()> {
     let mut context = nfc1::Context::new()?;
     let mut device = context.open()?;
 
     match device.initiator_select_passive_target(&nfc1::Modulation {
         modulation_type: nfc1::ModulationType::Iso14443a,
-        baud_rate: nfc1::BaudRate::Baud106,
+        baud_rate: nfc1::BaudRate::Baud424,
     }) {
-        Ok(target) => print!("Found a {}", target.to_string(false)?),
+        Ok(_target) => {
+            match device.initiator_transceive_bytes(&FIRST_CMD, 8, nfc1::Timeout::Default){
+                Ok(rx) => {
+                    if rx[0] == 0xaf {
+                        print!("Use 0xaf");
+                    }
+                },
+                Err(err) => print!("Error {}\n", err),
+            };
+        },
         Err(_) => print!("Error, or nothing found"),
     }
 
